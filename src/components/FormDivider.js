16a40a8f6dc7f4379cb3a3248168307e57765c6c
Mutiat Bashua <mutiatbashua@gmail.com>
2024-10-19 14:44:34 +0100

    Added login page

---
+++ src/components/FormDivider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const FormDivider = ({ text }) => {
+	return (
+		<div className="flex items-center justify-center gap-3">
+			<hr className="w-full border-t-1 border-gray-400"></hr>
+			<span>{text}</span>
+			<hr className="w-full border-t-1 border-gray-400"></hr>
+		</div>
+	);
+};
