72a6a83ec9fa0d5dde96eb4918f499fb92bad87e
Mutiat Bashua <mutiatbashua@gmail.com>
2024-10-21 20:25:18 +0100

    updated redirect uri on signup

--- src/modules/auth/pages/Signup.jsx
+++ src/modules/auth/pages/Signup.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Signup = () => {
 	const handleGithubLogin = () => {
 		const clientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-		const redirectUri = "http://localhost:3000/auth/github";
+		const redirectUri = "https://opennestafrica.netlify.app/auth/github";
 		const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
 		window.location.href = githubAuthUrl;
 	};
