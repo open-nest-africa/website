7908272f5e4af6872a52091104ebc57b8e7de44d
Younes <younes.abderral@gmail.com>
2025-05-06 15:46:41 +0100

    adding routes and adding new design to the hoover

--- src/modules/resources/Developers.jsx
+++ src/modules/resources/Developers.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import ContributionGuide from "../../components/ContributionGuide";
 import { contributionData } from "../../Data";
+import Layout from "../../components/layout/Layout";
 
 const Developers = () => {
-	const developerData = contributionData.find(
-		(contribution) => contribution.role === "Developers"
-	);
+  const developerData = contributionData.find(
+    (contribution) => contribution.role === "Developers"
+  );
 
-	return (
-		<ContributionGuide
-			data={developerData.contributions}
-			role={developerData.role}
-			icon={developerData.icon}
-			info={developerData.intro}
-			videoContent={!!developerData.videoTutorial}
-			videoText={developerData.videoTutorial?.description}
-			videoLink={developerData.videoTutorial?.link}
-			summaryTitle={developerData.remarks.title}
-			summaryContent1={developerData.remarks.content1}
-			summaryContent2={developerData.remarks.content2}
-		/>
-	);
+  return (
+    <Layout t>
+      <ContributionGuide
+        data={developerData.contributions}
+        role={developerData.role}
+        icon={developerData.icon}
+        info={developerData.intro}
+        videoContent={!!developerData.videoTutorial}
+        videoText={developerData.videoTutorial?.description}
+        videoLink={developerData.videoTutorial?.link}
+        summaryTitle={developerData.remarks.title}
+        summaryContent1={developerData.remarks.content1}
+        summaryContent2={developerData.remarks.content2}
+      />
+    </Layout>
+  );
 };
 
 export default Developers;
--- src/modules/resources/Resources.jsx
+++ src/modules/resources/Resources.jsx
@@ -1,72 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Overview from "./Overview";
-import TechnicalWriters from "./TechnicalWriters";
-import Developers from "./Developers";
-import Designers from "./Designers";
+
 import Layout from "../../components/layout/Layout";
-import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
-import { useLocation } from "react-router-dom";
 
 const Resources = () => {
-  const location = useLocation();
-  const [tab, setTab] = useState(0);
-
-  useEffect(() => {
-    if (location.state?.initialTab !== undefined) {
-      setTab(location.state.initialTab);
-    }
-  }, [location.state]);
-
-  const resources = [
-    { name: "Overview", component: <Overview key="1" /> },
-    { name: "Technical Writing", component: <TechnicalWriters key="2" /> },
-    { name: "Developer Guide", component: <Developers key="3" /> },
-    { name: "Design Guide", component: <Designers key="4" /> },
-  ];
-
-  const handleNext = () => {
-    setTab((prev) => (prev + 1) % resources.length);
-  };
-
-  const handlePrevious = () => {
-    setTab((prev) => (prev === 0 ? resources.length - 1 : prev - 1));
-  };
-
   return (
     <Layout>
-      {resources[tab].component}
-      <div
-        className={`${
-          tab === 0 ? "justify-end" : "justify-between"
-        } bg-[#E3DA35] w-full h-[118px] mb-[140px] flex items-center px-[99px]`}
-      >
-        {tab !== 0 && (
-          <div className="flex items-center gap-x-2">
-            <button onClick={handlePrevious}>
-              <ArrowLeftIcon className="w-5 h-5" />
-            </button>
-
-            <span className="text-2xl">Previous</span>
-            <label className="text-black text-[32px]">
-              {resources[tab - 1].name}
-            </label>
-          </div>
-        )}
-        {tab !== resources.length - 1 && (
-          <div className="flex items-center gap-x-2">
-            <span className="text-2xl">Next</span>
-            <label className="text-black font-medium text-[32px]">
-              {resources[tab + 1].name}
-            </label>
-            <button
-              disabled={tab === resources.length - 1}
-              onClick={handleNext}
-            >
-              <ArrowRightIcon className="w-5 h-5" />
-            </button>
-          </div>
-        )}
-      </div>
+      <Overview />
     </Layout>
   );
 };
