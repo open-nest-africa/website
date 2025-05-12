01ed06f3296d2a059b642fed0fead612699d4541
Noimot Alabi <kikkyal@gmail.com>
2025-05-12 22:55:22 +0100

    chore: added a build

---
+++ dist/assets/Button-Btgts8C2.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CCZ-rFA1.js";const a=({text:e,icon:s,className:r,onClick:n})=>t.jsxs("button",{onClick:n,className:`${r} rounded-md ${s&&"flex items-center gap-x-5"}`,children:[s&&typeof s=="string"?t.jsx("img",{src:s,alt:"Icon"}):s,t.jsx("span",{children:e})]});export{a as B};
---
+++ dist/assets/Gitlab-DBmIOUhz.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CCZ-rFA1.js";const i=()=>t.jsx("div",{children:"Gitlab"});export{i as default};
