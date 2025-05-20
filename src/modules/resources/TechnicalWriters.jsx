e52f7a063da4a312d99b1e8ae8b356493c4bfe7d
Bashua Mutiat <mutiatbashua@gmail.com>
2025-05-20 21:41:33 +0100

    Revert " added dropdown menu do navigate between ressource page"

--- src/modules/resources/TechnicalWriters.jsx
+++ src/modules/resources/TechnicalWriters.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import ContributionGuide from "../../components/ContributionGuide";
 import { contributionData } from "../../Data";
-import Layout from "../../components/layout/Layout";
 
 const TechnicalWriters = () => {
-  const technicalWriterData = contributionData.find(
-    (contribution) => contribution.role === "Technical Writers"
-  );
+	const technicalWriterData = contributionData.find(
+		(contribution) => contribution.role === "Technical Writers"
+	);
 
-  return (
-    <Layout>
-      <ContributionGuide
-        data={technicalWriterData.contributions}
-        role={technicalWriterData.role}
-        icon={technicalWriterData.icon}
-        info={technicalWriterData.intro}
-        videoContent={!!technicalWriterData.videoTutorial}
-        videoText={technicalWriterData.videoTutorial?.description}
-        videoLink={technicalWriterData.videoTutorial?.link}
-        summaryTitle={technicalWriterData.remarks.title}
-        summaryContent1={technicalWriterData.remarks.content1}
-        summaryContent2={technicalWriterData.remarks.content2}
-      />
-    </Layout>
-  );
+	return (
+		<ContributionGuide
+			data={technicalWriterData.contributions}
+			role={technicalWriterData.role}
+            icon={technicalWriterData.icon}
+			info={technicalWriterData.intro}
+			videoContent={!!technicalWriterData.videoTutorial}
+			videoText={technicalWriterData.videoTutorial?.description}
+			videoLink={technicalWriterData.videoTutorial?.link}
+			summaryTitle={technicalWriterData.remarks.title}
+			summaryContent1={technicalWriterData.remarks.content1}
+			summaryContent2={technicalWriterData.remarks.content2}
+		/>
+	);
 };
 
 export default TechnicalWriters;
