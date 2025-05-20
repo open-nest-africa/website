6ed4a2a6a724baf44f974cb74c220f527dacb35c
Bashua Mutiat <mutiatbashua@gmail.com>
2025-05-20 21:41:33 +0100

    Revert " added dropdown menu do navigate between ressource page"

--- src/modules/resources/Developers.jsx
+++ src/modules/resources/Developers.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import ContributionGuide from "../../components/ContributionGuide";
 import { contributionData } from "../../Data";
-import Layout from "../../components/layout/Layout";
 
 const Developers = () => {
-  const developerData = contributionData.find(
-    (contribution) => contribution.role === "Developers"
-  );
+	const developerData = contributionData.find(
+		(contribution) => contribution.role === "Developers"
+	);
 
-  return (
-    <Layout>
-      <ContributionGuide
-        data={developerData.contributions}
-        role={developerData.role}
-        icon={developerData.icon}
-        info={developerData.intro}
-        videoContent={!!developerData.videoTutorial}
-        videoText={developerData.videoTutorial?.description}
-        videoLink={developerData.videoTutorial?.link}
-        summaryTitle={developerData.remarks.title}
-        summaryContent1={developerData.remarks.content1}
-        summaryContent2={developerData.remarks.content2}
-      />
-    </Layout>
-  );
+	return (
+		<ContributionGuide
+			data={developerData.contributions}
+			role={developerData.role}
+			icon={developerData.icon}
+			info={developerData.intro}
+			videoContent={!!developerData.videoTutorial}
+			videoText={developerData.videoTutorial?.description}
+			videoLink={developerData.videoTutorial?.link}
+			summaryTitle={developerData.remarks.title}
+			summaryContent1={developerData.remarks.content1}
+			summaryContent2={developerData.remarks.content2}
+		/>
+	);
 };
 
 export default Developers;
