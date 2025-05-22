a615d4bafb3c62df8be3fd81c8d7170a8aceb449
Younes <younes.abderral@gmail.com>
2025-05-22 09:33:43 +0100

    added dropdown menu to ressource page

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../Button";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isResourcesOpen, setIsResourcesOpen] = useState(false);
+
+  useEffect(() => {
+    setIsResourcesOpen(false);
+  }, []);
   return (
     <header className="sticky top-0 z-50 bg-white px-[112px] py-6 shadow">
       <nav className="flex items-center justify-between">
@@ -16,15 +21,51 @@ const Header = () => {
           <li className="flex items-center gap-x-1">
             use cases <ChevronDownIcon className="w-6 h-6" />
           </li>
-          <li>
-            <Link to="/resources" className="flex items-center gap-x-1">
+          <li className="relative">
+            <div
+              className="flex items-center gap-x-1 cursor-pointer"
+              onClick={(e) => {
+                setIsResourcesOpen(!isResourcesOpen);
+              }}
+              onMouseEnter={() => setIsResourcesOpen(true)}
+            >
               Resources <ChevronDownIcon className="w-6 h-6" />
-            </Link>
+            </div>
+            {isResourcesOpen && (
+              <div
+                className="absolute top-full left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50"
+                onClick={(e) => e.stopPropagation()}
+                onMouseLeave={() => setIsResourcesOpen(false)}
+              >
+                <Link to={"/resources"}>
+                  <button className="b  lock w-full text-left px-4 py-2 text-sm text-gray-700   hover:bg-[#0C9EDD] hover:text-white">
+                    Overview
+                  </button>
+                </Link>
+
+                <Link to={"/resources/technical-writing"}>
+                  <button
+                    className="block w-full text-left px-4 py-2 text-sm text-gray-700  hover:bg-[#0C9EDD] hover:text-white"
+                    color=""
+                  >
+                    Technical-Writing
+                  </button>
+                </Link>
+                <Link to={"/resources/developers"}>
+                  <button className="block w-full text-left px-4 py-2 text-sm text-gray-700  hover:bg-[#0C9EDD] hover:text-white">
+                    Development
+                  </button>
+                </Link>
+                <Link to={"/resources/designers"}>
+                  <button className="block w-full text-left px-4 py-2 text-sm text-gray-700  hover:bg-[#0C9EDD] hover:text-white">
+                    Design
+                  </button>
+                </Link>
+              </div>
+            )}
           </li>
           <li>
-            <Link to="/about-us">
-              About us
-            </Link>
+            <Link to="/about-us">About us</Link>
           </li>
           <li>What's new</li>
         </ul>
