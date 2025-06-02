831ad8ab40037834977ee8b36f03fbee77909c9f
GifftyCode <ulokangozi@yahoo.com>
2025-06-02 16:19:56 +0100

    Feat: Implement Google Auth (Sign Up & Sign In) with Firebase

--- src/modules/auth/pages/Signup.jsx
+++ src/modules/auth/pages/Signup.jsx
@@ -2,25 +2,48 @@ import React from "react";
 import AuthLayout from "../component/AuthLayout";
 import Button from "../../../components/Button";
 import { Link } from "react-router-dom";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../../firebase/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const navigate = useNavigate();
+
   const handleGithubLogin = () => {
-		const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
-		const redirectUri = import.meta.env.VITE_GITHUB_REDIRECT_URL; 
-		const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
-		window.location.href = githubAuthUrl;
-	};
+    const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_GITHUB_REDIRECT_URL;
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
+    window.location.href = githubAuthUrl;
+  };
+
+  const handleGoogleSignup = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const token = await user.getIdToken();
+      console.log(token);
+
+      localStorage.setItem("accessToken", token);
+
+      console.log("User:", user);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("Error during sign-up:", error);
+    }
+  };
+
   return (
     <AuthLayout title="Sign up" className="flex-col items-center">
       <div>
         <div className="space-y-4 flex flex-col justify-center items-center w-[448px] pt-10">
-        <Button
-        onClick={handleGithubLogin}
+          <Button
+            onClick={handleGithubLogin}
             text="Continue with Github"
             icon="/images/github-logo.svg"
             className="bg-primary text-[#344054] text-base font-semibold align-center py-4 px-24 w-full"
           />
           <Button
+            onClick={handleGoogleSignup}
             text="Continue with Google"
             icon="/images/google-logo.svg"
             className="bg-primary text-[#344054] text-base font-semibold align-center py-4 px-24 w-full"
