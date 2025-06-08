08e52f9c1b7ce3c88adaf25ad89b27adaeebb0f5
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-06-08 15:50:59 +0100

    feat: update hover effect for links and auth buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
+const Button = ({ text, icon, className, onClick, type }) => {
+  const authHoverStyles =
+    type === "auth" ? "hover:border hover:border-blue" : "";
 
-const Button = ({ text, icon, className, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`${className} rounded-md transform transition-all duration-300 ease-in-out hover:scale-105 hover:bg-gradient-to-r hover:from-blue/90 hover:to-blue/50  hover:shadow-xl active:scale-95 before:absolute before:inset-0 before:bg-gradient-to-r before:from-transparent before:via-white/20 before:to-transparent before:translate-x-[-100%] hover:before:translate-x-[100%] before:transition-transform before:duration-700 overflow-hidden ${
+      className={`${className} rounded-md transform transition-all duration-300 ease-in-out hover:scale-[104%] hover:shadow-lg active:scale-95 before:absolute before:inset-0 before:bg-gradient-to-r before:from-transparent before:via-white/20 before:to-transparent before:translate-x-[-100%] hover:before:translate-x-[100%] before:transition-transform before:duration-700 overflow-hidden ${authHoverStyles} ${
         icon && "flex items-center gap-x-5"
       }`}
     >
