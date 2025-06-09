77ad089aa53aa3c09d49b957286b7571a34de762
oliverTwist2 <oliverikegah@gmail.com>
2025-06-09 20:59:37 +0100

    Added nodemon as a dev dependency, changed the dev start script to use npm run dev in backend. Removed process.exiit() in database connection so server doesn't break if mongo connection fails

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/open-nest-africa";
+    const MONGODB_URI = process.env.MONGODB_URI 
     await mongoose.connect(MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1);
   }
+  //process.exit(1);
 };
 
 module.exports = connectDB;
